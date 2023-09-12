6178ef2650447f876a1b7b643c0291658e212416
pierosimonet <simonetto.piero@gmail.com>
2023-09-12 10:59:44 +0200

    added the services

--- scripts/confidence_control.py
+++ scripts/confidence_control.py
@@ -10,11 +10,12 @@ from geometry_msgs.msg import PoseStamped, Pose
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionGoal
 import actionlib
 
-odom_topic = '/odom'
-frame_destinatin = 'wcias_base_footprint'
-goal_topic = '/move_base/goal/'
+from std_srvs.srv import Empty 
 
-navigation_name = "move_base"
+odom_topic = '/odom'
+goal_topic = '/move_base/goal/'
+delta_distance = 0.5 # [m]
+Hz = 16
 
 def odom_callback(data: Odometry):
     global odom_position
@@ -28,39 +29,50 @@ def setup_listeners():
     rospy.Subscriber(odom_topic, Odometry, odom_callback)
     rospy.Subscriber(goal_topic, MoveBaseActionGoal, goal_callback)
 
+def setup_parameters():
+    odom_topic = rospy.get_param('~odom_topic', odom_topic)
+    goal_topic = rospy.get_param('~goal_topic', goal_topic)
+    delta_distance = rospy.get_param('~delta_distance', delta_distance)
+    Hz = rospy.get_param('~hz', Hz)
+
+def setup_services():
+    global request_new_target
+    rospy.wait_for_service('/navigation/request_new_target')
+    request_new_target = rospy.ServiceProxy('/navigation/request_new_target', Empty)
+
 def init_globals():
-    global  current_destination, path, odom_position, state
-    state = 0
+    global  current_destination, path, odom_position
     odom_position = Odometry()
     path = Path()
     current_destination = PoseStamped()
     current_destination.header.frame_id = "wcias_odom"
 
 
-def set_controller_state():
-    global odom_position, current_destination, movebase_client,  state
+def check_distance_to_current_destination():
+    global odom_position, current_destination
 
     d = math.sqrt( (odom_position.pose.pose.position.x - current_destination.pose.position.x) **2 +\
                    (odom_position.pose.pose.position.y - current_destination.pose.position.y) **2 )
 
-    print(d)
+    return d < delta_distance
 
 
 def main():
     rospy.init_node('print_d')
-
-    global movebase_client
-    movebase_client = actionlib.SimpleActionClient(navigation_name, MoveBaseAction)
-    movebase_client.wait_for_server()
-
+    
+    setup_parameters()
     init_globals()
     setup_listeners()
+    setup_services()
 
     # TODO : update these as parameters
-    rate = rospy.Rate(16)
+    rate = rospy.Rate(Hz)
+
+    global request_new_target
 
     while not rospy.is_shutdown():
-        set_controller_state()
+        if check_distance_to_current_destination():
+            request_new_target()
         rate.sleep()
     
 if __name__ == '__main__':
