ac80c0cc0044a632f8503847fb7dc27fcc090ce4
pierosimonet <simonetto.piero@gmail.com>
2024-03-05 16:40:07 +0100

    updated setting

--- src/ConfidenceControl.cpp
+++ src/ConfidenceControl.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "confidence_control");
-    prox::confidence_control* cf = new prox::confidence_control();
+    wheelchair_iaslab_target_generator::confidence_control* cf = new wheelchair_iaslab_target_generator::confidence_control();
     cf->run();
     return 0;
 }
--- src/TargetGenerator.cpp
+++ src/TargetGenerator.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "target_generator");
-    prox::target_generator* tg = new prox::target_generator();
+    wheelchair_iaslab_target_generator::target_generator* tg = new wheelchair_iaslab_target_generator::target_generator();
     tg->run();
     return 0;
 }
